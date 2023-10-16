9c72dd92bffd4fd646e2772747cdc70885cfd64f
gustavo.rocha <gustavorocha.net@hotmail.com>
2023-10-16 19:42:14 -0300

    implantação

---
+++ trilha-net-tdd-testes-unitarios-desafio-dio/NewTalents/NewTalents/Calculadora.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewTalents
+{
+    public class Calculadora
+    {
+        private List<string> _listaHistorico;
+        private string _data;
+
+        public Calculadora(string data)
+        {
+            _listaHistorico = new List<string>();
+            _data = data;
+        }
+
+        public int Somar(int val1, int val2)
+        {
+            int res = val1 + val2;
+            _listaHistorico.Insert(0, "Res: " + res + " - data: " + _data);
+            return res;
+        }
+
+        public int Subtrair(int val1, int val2)
+        {
+            int res = val1 - val2;
+            _listaHistorico.Insert(0, "Res: " + res + " - data: " + _data);
+            return res;
+        }
+        
+        public int Multiplicar(int val1, int val2)
+        {
+            int res = val1 * val2;
+            _listaHistorico.Insert(0, "Res: " + res + " - data: " + _data);
+            return res;
+        }
+        
+        public int Dividir(int val1, int val2)
+        {
+            int res = val1 / val2;
+            _listaHistorico.Insert(0, "Res: " + res + " - data: " + _data);
+            return res;
+        }
+
+        public List<string> Historico()
+        {
+            _listaHistorico.RemoveRange(3, _listaHistorico.Count - 3);
+            return _listaHistorico;
+        }
+    }
+}
